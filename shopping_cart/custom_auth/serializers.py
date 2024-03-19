2ded38955dc10144905e621ea15c3b117c72b59c
Nitish Kumar <nitishkumarchauhan1996@gmail.com>
2024-03-19 14:14:57 +0530

    chores: remove unused imports

--- shopping_cart/custom_auth/serializers.py
+++ shopping_cart/custom_auth/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
